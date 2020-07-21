ccc0138c4d9080a12390cb12d6d4cb9cd14def5d
Ayush <ayushbisht5663@gmail.com>
2020-07-21 11:21:42 +0530

    j8y18lte: include proprietary/ dir to PATH vendor

---
+++ j8y18lte-vendor.mk
@@ -0,0 +1,10 @@
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/bin,vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/etc,vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/firmware,vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/lib,vendor/lib)
+
+

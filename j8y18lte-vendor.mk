7b25e479881d6853a929ae42780fc97ef3298c08
Ayush <ayushbisht5663@gmail.com>
2020-12-19 15:17:51 +0530

    j8y18lte: rebase from master

--- j8y18lte-vendor.mk
+++ j8y18lte-vendor.mk
@@ -1,10 +1,16 @@
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/app,vendor/app)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/bin,vendor/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/etc,vendor/etc)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/firmware,vendor/firmware)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/framework,vendor/framework)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/lib,vendor/lib)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/media,vendor/media)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/rfs,vendor/rfs)

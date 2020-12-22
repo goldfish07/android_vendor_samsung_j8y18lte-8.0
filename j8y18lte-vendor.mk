6b6e1c583d87e7a38df460236be9b3f72ed49d36
Ayush <ayushbisht5663@gmail.com>
2020-12-22 16:16:59 +0530

    j8y18lte: follow new vendor structure

--- j8y18lte-vendor.mk
+++ j8y18lte-vendor.mk
@@ -1,4 +1,17 @@
 
+# For System
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/framework,system/framework)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/lib,system/lib)
+
+
+#For Vendor
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/app,vendor/app)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/bin,vendor/bin)

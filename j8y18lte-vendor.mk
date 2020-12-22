1bd52fc8d3d1acec88c9a3950d2a44273359f3fb
Ayush <ayushbisht5663@gmail.com>
2020-12-22 16:26:04 +0530

    j8y18lte: update new vendor PATH

--- j8y18lte-vendor.mk
+++ j8y18lte-vendor.mk
@@ -12,18 +12,18 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/pr
 
 
 #For Vendor
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/app,vendor/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/app,vendor/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/bin,vendor/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/bin,vendor/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/etc,vendor/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/etc,vendor/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/firmware,vendor/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/firmware,vendor/firmware)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/framework,vendor/framework)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/framework,vendor/framework)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/lib,vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/lib,vendor/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/media,vendor/media)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/media,vendor/media)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/rfs,vendor/rfs)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/j8y18lte/proprietary/vendor/rfs,vendor/rfs)
